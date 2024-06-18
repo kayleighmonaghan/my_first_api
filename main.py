b017e7b62a56c91a40bed908e2552e05a0294126
kayleigh_monaghan <kayleigh.monaghan@icloud.com>
2024-06-18 15:58:45 +0100

    initialised empty items list along with post and get methods

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+
 app = FastAPI()
+
+items = []
 
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
+@app.post("/items")
+def create_item(item: str):
+    # add 'item' input to items list:
+    items.append(item)
+    return item
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int) -> str:
+    # retrieve an item from the list
+    item = items[item_id]
+    return item
