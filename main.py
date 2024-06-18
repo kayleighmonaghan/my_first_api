4e3b450b8f712f6989d048b857c9f4d5f66fb2b5
kayleigh_monaghan <kayleigh.monaghan@icloud.com>
2024-06-18 17:01:34 +0100

    import pydantic, create Item class for to-do list

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    # extend pydantic basemodel to create Item class; initialise Item's attributes
+    text: str = None
+    is_done: bool = False
 
 items = []
 
@@ -9,7 +15,7 @@ def root():
     return {"Hello": "World"}
 
 @app.post("/items")
-def create_item(item: str):
+def create_item(item: Item):
     # add 'item' input to items list:
     items.append(item)
     return items
@@ -20,7 +26,7 @@ def list_items(limit: int = 10):
     return items[0:limit]
 
 @app.get("/items/{item_id}")
-def get_item(item_id: int) -> str:
+def get_item(item_id: int) -> Item:
     # retrieve an item from the list; if it does not exist then raise an error message
     if item_id < len(items):
         return items[item_id]
